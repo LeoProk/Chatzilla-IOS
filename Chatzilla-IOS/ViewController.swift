042a94686ab594916ab5a79d7c4105a09b34247c
LeoProk <just_beta@hotmail.com>
2018-04-25 23:52:27 +0300

    added airbase

--- Chatzilla-IOS/ViewController.swift
+++ Chatzilla-IOS/ViewController.swift
@@ -11,15 +11,40 @@ class ViewController: MessagesViewController {
 
     //array of empty messages
     var messages : [MessageType] = []
+    var firebaseData : Array<ChatMessage> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //assign the messagekit
+        //assign the messagekit to thew 
         messagesCollectionView.messagesDataSource = self as? MessagesDataSource
         messagesCollectionView.messagesLayoutDelegate = self as? MessagesLayoutDelegate
         messagesCollectionView.messagesDisplayDelegate = self as? MessagesDisplayDelegate
+        //create dispatch group to update the tab view when done fetching all the
+        //data from firebase
+        let dispatchGroup = DispatchGroup()
+        //populate the array with parks from firebase
+        let ref = Database.database().reference(fromURL: "https://chatzilla-ad20b.firebaseio.com/")
+        dispatchGroup.enter()
+        ref.observe(DataEventType.value, with: { (snapshot) in
+            _ = snapshot.value as? [String : AnyObject] ?? [:]
+            //for loop that loops over snapshot data to poulate new park
+            //add the park to parks array
+            for fireObj in snapshot.children{
+                //create new park from the firebase info
+                let parkSnap = ChatMessage(dataSnap : fireObj as! DataSnapshot)
+                //adds the park to the park list
+                //if there still parks left continue to loop otherwise
+                //end the dispatch
+                if self.firebaseData.count < snapshot.childrenCount - 1{
+                    self.firebaseData.append(parkSnap)
+                    //self.messages.append(parkSnap)
+                }else {
+                    dispatchGroup.leave()
+                }
+            }
+        })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
