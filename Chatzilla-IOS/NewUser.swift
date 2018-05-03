cb94fba403bae86959f3cc8db7f6999b78d4a08e
LeoProk <just_beta@hotmail.com>
2018-05-03 06:28:16 +0300

    added user creation

--- Chatzilla-IOS/NewUser.swift
+++ Chatzilla-IOS/NewUser.swift
@@ -10,15 +10,18 @@ import Foundation
 import Firebase
 
 class NewUser : UIViewController {
+    var ref : DatabaseReference!
     @IBOutlet weak var num: UILabel!
     @IBOutlet weak var name: UILabel!
+    @IBAction func acceptClicked(_ sender: Any) {
+        //create new user using phone number and nickname
+        ref.child("users").child(num.text!).setValue(name.text!)
+        
+    }
     
     override func viewDidLoad() {
         //get referance to firedatabase
-        let ref = Database.database().reference(fromURL: "https://chatzilla-ad20b.firebaseio.com/")
-        //create new user using phone number and nickname
-        ref.child("users").child("<#T##pathString: String##String#>").setValue("<#T##value: Any?##Any?#>")
+        ref = Database.database().reference(fromURL: "https://chatzilla-ad20b.firebaseio.com/")
     }
-
     
 }
